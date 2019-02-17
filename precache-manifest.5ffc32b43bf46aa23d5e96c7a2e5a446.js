f60f539f4521bb5db189559a3c0a2f30f396f5ad
Lori Culberson <bigskybandit@sbcglobal.net>
2019-02-17 14:16:01 -0500

    Updates

---
+++ precache-manifest.5ffc32b43bf46aa23d5e96c7a2e5a446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce353e3bac3abdc3dba",
+    "url": "/loriculberson/gift-giver-tdd/static/js/runtime~main.8ce353e3.js"
+  },
+  {
+    "revision": "58bf16b13ef547ec6f0d",
+    "url": "/loriculberson/gift-giver-tdd/static/js/main.58bf16b1.chunk.js"
+  },
+  {
+    "revision": "47108b3238107d42d02d",
+    "url": "/loriculberson/gift-giver-tdd/static/js/2.47108b32.chunk.js"
+  },
+  {
+    "revision": "58bf16b13ef547ec6f0d",
+    "url": "/loriculberson/gift-giver-tdd/static/css/main.49ed5172.chunk.css"
+  },
+  {
+    "revision": "634f5b72a6df9cbfe77fd2059b1356d0",
+    "url": "/loriculberson/gift-giver-tdd/index.html"
+  }
+];
